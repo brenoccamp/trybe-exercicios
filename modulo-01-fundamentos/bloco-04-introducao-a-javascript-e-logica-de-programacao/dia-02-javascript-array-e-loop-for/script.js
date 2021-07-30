07a77c4b17783acafc58851822bbba4f330a5425
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-07-30 12:05:40 -0400

    Cria script.js do dia de aula 02 de javascript

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -0,0 +1,37 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+// console.log(tasksList.length);
+
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+
+// let searchForLastTask = tasksList [tasksList.length - 1]
+// console.log(searchForLastTask); 
+// tasksList.push('Fazer exercícios da Trybe'); //Adiciona mais uma tarefa(task) ao array
+// console.log(tasksList);
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe'];
+// tasksList.pop(); //Remove a última tarefa
+// console.log(tasksList);
+
+// [ 'Tomar café', 'Reunião', 'Brincar com o cachorro'];
+// console.log(tasksList.indexOf('Fazer exercícios da Trybe'))
+
+// Exercícios para fixar:
+// Exercício-1 Obtenha o valor "Serviços" do array menu:
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+// console.log(menuServices);
+
+// Exercício-2 Procure o índice do valor "Portfólio" do array menu:
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+// Exercício-3 Adicione o valor "Contato" no final do array menu:
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato');
+console.log(menu);
