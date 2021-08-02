bbbc6120400ef94d98ec7dc8831a1411640e9f2b
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-02 18:23:30 -0400

    Resolve exercicios Parte 1 e Parte 2 do bloco 4-Sessao de exercicios

--- modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/scrip.js
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/scrip.js
@@ -113,3 +113,67 @@
 //   return a % b;
 // };
 // console.log(modul(a, b))
+
+
+//EXERCÌCIOS DO BLOCO 4 - PARTE 1 - OBJETOS E FOR/IN
+// Usando o objeto abaixo, faça os exercícios a seguir:
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+// 1- Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+// console.log('Bem vinda ' + info.personagem)
+
+// 2- Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+info.recorrente = 'Sim'
+// console.log(info)
+
+// 3- Faça um for/in que mostre todas as chaves do objeto.
+// for (let keys in info) {
+//     console.log(keys)
+// }
+
+// 4- Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+// for (let keys in info) {
+//     console.log(info[keys])
+// }
+
+// 5- Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+// }
+// info2.recorrente = 'Sim'
+// for (let key in info) {
+//     for (let key in info2) {        
+//     }
+//     console.log(info[key] + ' e ' + info2[key])
+// }
+
+//EXERCÌCIOS DO BLOCO 4 - PARTE 2 - FUNÇÕES
+// 1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+// Exemplo de palíndromo: arara .
+// verificaPalindrome('arara') ;
+// Retorno esperado: true
+// verificaPalindrome('desenvolvimento') ;
+// Retorno esperado: false
+// function palindrome(word) { //MODO 1 PARA RESOLVER ESSE EXERCÌCIO
+//     for (let index in word) {
+//         if (word[index] !== word[(word.length - 1) - index]) {
+//             return false + '. Essa palavra não é um palíndromo.'
+//         }
+//     }
+//     return true + '. Essa palavra é um palíndromo'
+// }
+// function verifyPalindrome(string) { //MODO 2 PARA RESOLVER ESSE EXERCÍCIO
+//     let reverse = string.split('').reverse().join('')
+//     if (string === reverse) {
+//         return true + '. Essa palavra é um palíndromo'
+//     } else {
+//         returnfalse + '. Essa palavra não é um palíndromo'
+//     }
+// }
+// console.log(palindrome('cabo'))
+// console.log(verifyPalindrome('arara'))
