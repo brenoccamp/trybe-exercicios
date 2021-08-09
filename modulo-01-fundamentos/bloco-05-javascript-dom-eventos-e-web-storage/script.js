fbfc0356057e2bc5dc71b84fbdfe537c46369e4e
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-09 17:29:07 -0400

    move arquivos para suas respectivas pastas

--- modulo-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/script.js
+++
@@ -1,6 +0,0 @@
-const paragraph = document.getElementById("paragraph");
-      paragraph.style.color = "red";
-
-document.getElementById("page-title").innerText = "The Big Bang Theory"
-document.getElementById("second-paragraph").innerText = "The Big Bang Theory é uma SitCom que retrata a vida de personagens fictícios de maneira cômica e caricata. Sheldon Cooper pode ser citado como a peça central do enredo e junto com seus 3 amigos, Leonard, Howard, e Rajesh, eles pode te matar de rir. A série relata situações da vida desses personagens que vivem e tenta trazer para o espectador o ambiente científico em que eles vivem."
-document.getElementById('subtitle').innerText = 'Uma SitCom sobre a vida de 4 excêntricos cientistas!'
