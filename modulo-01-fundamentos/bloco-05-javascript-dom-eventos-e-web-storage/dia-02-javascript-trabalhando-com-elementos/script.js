ec9395b7f188fbbefe8eebc526ad80b7e8ea5e52
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-10 17:40:03 -0400

    Resolve exercicios 1 e 2 da lista de exercicios do bloco 5.2-trabalahndo com elementos

---
+++ modulo-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,11 @@
+//1
+const addTagH1 = document.createElement('h1');
+addTagH1.innerText = 'Exercício 5.2 - JavaScript DOM';
+document.body.appendChild(addTagH1);
+
+//2
+const addTagMain = document.createElement('main');
+addTagMain.className = 'main-content';
+document.body.appendChild(addTagMain);
+
+//3
