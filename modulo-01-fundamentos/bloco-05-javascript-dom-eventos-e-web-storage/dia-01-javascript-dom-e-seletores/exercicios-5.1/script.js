63b84d1cfc9feac3375a47111a2480a0dec00da5
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-10 10:13:58 -0400

    Resolve exercicios do 1 ao 6 bloco-5.1

--- modulo-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercicios-5.1/script.js
+++ modulo-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercicios-5.1/script.js
@@ -1,35 +1,39 @@
 //Exercício 1
-function exercicio1 () {
+function changeText () {
     document.getElementsByTagName('p')[0].innerHTML = 'Trabalhando em uma empresa internacional e morando no brasil.'
 }
+changeText ();
 
 //Exercício 2
-function exercicio2 () {
+function changeBackgroundColor() {
     document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)'
 }
+changeBackgroundColor ();
 
 //Exercício 3
-function exercicio3 () {
+function changeInternalColor () {
     document.getElementsByTagName('section')[0].style.backgroundColor = 'white';
 }
+changeInternalColor ();
 
 //Exercício 4
-function exercicio4 () {
+function toFixText () {
     document.getElementsByTagName('h1')[0].innerHTML = 'Exercício 5.1 - JavaScript';
 }
+toFixText ();
 
 //Exercício 5
-function exercicio5 () {
-    document.getElementsByTagName('p')[0].style.textTransform = 'UpperCase';
+function toUpperText () {
+   let upperText = document.getElementsByTagName('p')[0];
+   upperText.innerHTML = upperText.innerHTML.toUpperCase(); 
 }
-
-function chamada () {
-    exercicio1();
-    exercicio2();
-    exercicio3();
-    exercicio4();
-    exercicio5();
-}
+toUpperText ();
 
 //Exercício 6
-// console.log(document.getElementsByTagName('')[0].innerHTML)
+function showAllTagsP () {
+    let showAllP = document.getElementsByTagName('p');
+    for (let i = 0; i < showAllP.length; i++) {
+        console.log(showAllP[i].innerHTML);        
+    }
+}
+showAllTagsP ();
