986aad0d9ed391d7bfdd90b86ddb8a0fc19eb47e
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-11 14:53:37 -0400

    Resolve exercicio 2 do exercicio de fixaçao do bloco 5.3-parte II - Conheça o addEventListener

--- modulo-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/mainfixacao.js
+++ modulo-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/mainfixacao.js
@@ -1,27 +1,36 @@
-const firstLi = document.getElementById('first-li');
-const secondLi = document.getElementById('second-li');
-const thirdLi = document.getElementById('third-li');
+const firstLi = document.getElementById('firstLi');
+const secondLi = document.getElementById('secondLi');
+const thirdLi = document.getElementById('thirdLi');
 const input = document.getElementById('input');
-const myWebpage = document.getElementById('my-spotrybefy');
+const myWebpage = document.getElementById('mySpotrybefy');
 
-/*
-1. Copie esse arquivo e edite apenas ele;
+// 1. Copie esse arquivo e edite apenas ele;
 
-2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+// 2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+function newClassTech (event) {
+    const techElement = document.querySelector('.tech');
+    techElement.classList.remove('tech');
+    event.target.classList.add('tech');
+    input.value = '';
+}
+firstLi.addEventListener('click', newClassTech);
+secondLi.addEventListener('click', newClassTech);
+thirdLi.addEventListener('click', newClassTech);
 
-3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech';
+// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+// com a classe 'tech';
 
-4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-redirecione para alguma página;
-4.1. Que tal redirecionar para seu portifólio?
 
-5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-a cor do mesmo;
+// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+// redirecione para alguma página;
+// 4.1. Que tal redirecionar para seu portifólio?
 
-Segue abaixo um exemplo do uso de event.target:
-*/
+// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo;
+
+// Segue abaixo um exemplo do uso de event.target:
+
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
