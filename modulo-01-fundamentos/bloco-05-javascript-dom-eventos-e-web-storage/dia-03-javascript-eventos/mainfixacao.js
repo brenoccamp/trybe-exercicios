f02218eda7012d0610de6e73b9226abef48bcfb6
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-11 17:29:51 -0400

    Resolve exercicio 3 dos exercicios de fixaçao do bloco 5.3-parte II

--- modulo-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/mainfixacao.js
+++ modulo-01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/mainfixacao.js
@@ -20,11 +20,16 @@ thirdLi.addEventListener('click', newClassTech);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+input.addEventListener ('input', function(event) {
+  let techElement = document.querySelector('.tech');
+  techElement.innerText = event.target.value;
+});
 
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
