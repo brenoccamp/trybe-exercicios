273ff1649d6fddb71959be48d2feb40640036e45
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-30 14:50:14 -0400

    Cria arquivo scriptParteII.js para resoluçao dos exercicios da parteII do bloco 7.2 - Objects

---
+++ modulo-01-fundamentos/bloco-07-introdução-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/scriptParteII.js
@@ -0,0 +1,47 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+
+// 1 - Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+// 2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+// 3 - Crie uma função para mostrar o tamanho de um objeto.
+// 4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+// 5 - Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte: 
+/*
+console.log(allLessons);
+{
+lesson1:
+  { materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã' },
+lesson2:
+  { materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+    turno: 'noite' },
+lesson3:
+  { materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite' }
+};
+*/
+
