5eb0e115b1550059826f8a8b53a587e56fd433b2
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-30 14:46:41 -0400

    Soluçao dos exercicios 1 e 2 da ParteI - bloco 7.2 - Objects

---
+++ modulo-01-fundamentos/bloco-07-introdução-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/scriptParteI.js
@@ -0,0 +1,61 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (order) => {
+  // Adicione abaixo as informações necessárias.
+  console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`);
+}
+
+customerInfo(order);
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+  const newBuyer = order.name = 'Luiz Silva';
+  const pizzas = Object.keys(order.order.pizza);
+  const drinks = order.order.drinks.coke.type;
+  const newTotal = order.payment.total = '50';
+  console.log(`Olá ${newBuyer}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${drinks} é R$ ${newTotal}`);
+}
+
+orderModifier(order);
+
+
+//Agora você vai fazer alguns exercícios de fixação.
+// Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+// Note que o parâmetro da função já está sendo passado na chamada da função.
+// Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
+// Modifique o nome da pessoa compradora.
+// Modifique o valor total da compra para R$ 50,00.
