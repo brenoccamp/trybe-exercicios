e18ecddb2639747283e8c77c5f27ceb6dc475579
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-31 14:03:14 -0400

    Soluçao exercicios 2 da parteI - bloco 7.3 - testes unitarios em js

--- modulo-01-fundamentos/bloco-07-introdução-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/exerciseParteI.js
+++ modulo-01-fundamentos/bloco-07-introdução-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitarios-em-javascript/exerciseParteI.js
@@ -10,7 +10,6 @@ function sum(a, b) {
 
   return a + b;
 }
-const expected = sum(0, 5);
 // implemente seus testes aqui
 // 1 - A função sum(a, b) retorna a soma do parâmetro a com o b
 assert.strictEqual(typeof sum, 'function');
@@ -22,6 +21,44 @@ assert.strictEqual(sum(4, 5), 9);
 assert.strictEqual(sum(0, 0), 0);
 
 // 4 - Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+assert.throws(() => {
+  sum(4, '5');
+})
 // 5 - Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
-assert.strictEqual(sum(4, '5'), 9);
+assert.throws(() => {
+  sum(4, '5');
+}, /^Error: parameters must be numbers$/);
 
+
+// A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+// const assert = require('assert');
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  // console.log(typeof arr)
+  // console.log(typeof item)
+  // console.log(typeof newArr)
+  return newArr;
+}
+// myRemove([1, 2, 3, 4], 3)
+// implemente seus testes aqui
+// 1 - Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+// assert.strictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4])
+assert.strictEqual(typeof myRemove, 'function');
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]); // deepStrictEqual testa se dois objetos e seus objetos filhos são iguais, usando o operador ===.
+
+// 2 - Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+// 3 - Verifique se o array passado por parâmetro não sofreu alterações
+const myList = [1, 2, 3, 4, 5, 6];
+myRemove(myList, 5);
+assert.deepStrictEqual(myList, [1, 2, 3, 4, 5, 6]);
+
+// 4 - Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+const test = myRemove([1, 2, 3, 4], 5);
+assert.deepStrictEqual(test, [1, 2, 3, 4]);
