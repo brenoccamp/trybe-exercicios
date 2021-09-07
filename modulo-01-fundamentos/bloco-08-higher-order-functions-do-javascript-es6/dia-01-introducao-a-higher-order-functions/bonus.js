8dfc9034dd2fb2d9e082e490ab8e61a053a307ae
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-06 21:54:38 -0400

    exercicios 1, 2 e 3 do bonus

--- modulo-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/bonus.js
+++ modulo-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/bonus.js
@@ -27,8 +27,39 @@ const battleMembers = { mage, warrior, dragon };
 
 // 1 - Crie uma função que retorna o dano do dragão.
 // O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
-const dragDamage = () => {
-  const aleatoryDamage = Math.floor(Math.random() * (51 - 15)) + 15;
-  console.log(aleatoryDamage);
+const dragDamage = (dragon) => {
+  const dragStrength = dragon.strength;
+  const aleatoryDamage = Math.floor(Math.random() * ((dragStrength + 1) - 15)) + 15;
+  return aleatoryDamage;
 }
-dragDamage();
+dragDamage(dragon);
+
+// 2 - Crie uma função que retorna o dano causado pelo warrior .
+// O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
+const warriorDamage = (warrior) => {
+  const maxDamage = warrior.weaponDmg * warrior.strength;
+  const warriorStrength = warrior.strength;
+  const aleatoryDamage = Math.floor(Math.random() * ((maxDamage + 1) - warriorStrength) + warriorStrength);
+  return aleatoryDamage;
+}
+warriorDamage(warrior);
+
+// 3 - Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+// O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+// A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
+const mageDamage = (mage) => {
+  const maxDamage = mage.intelligence * 2;
+  const mageIntelligence = mage.intelligence;
+  let magesMana = mage.mana;
+  const bout = {
+    aleatoryDamage: 'Não possui mana suficiente',
+    spentMana: 0,
+  };
+  if (magesMana > 15) {
+    bout.aleatoryDamage = Math.floor(Math.random() * ((maxDamage + 1) - mageIntelligence) + mageIntelligence);
+    bout.spentMana = 15;
+  }
+  console.log(bout)
+  return bout;
+}
+mageDamage(mage);
