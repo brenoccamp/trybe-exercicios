8c858d141cda0b426385c6fab344c41d821c5c85
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-08 18:33:56 -0400

    Soluçao do exercicio 7 - dia 8.3

---
+++ modulo-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-map-e-filter/exercise7.js
@@ -0,0 +1,14 @@
+const books = require('./base');
+const assert = require('assert');
+const { workerData } = require('worker_threads');
+// 7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+// Dica: cada inicial termina com um ponto.
+
+const expectedResult = 'O Senhor dos Anéis';
+
+function authorWith3DotsOnName() {
+  // escreva seu código aqui
+  return books.find((book) => book.author.name.split(' ').filter((letter) => letter.endsWith('.')).length === 3).name;
+}
+console.log(authorWith3DotsOnName())
+assert.deepStrictEqual(authorWith3DotsOnName(), expectedResult);
