c4a132bce170c0413ee3dd3638674efecc97e14b
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-09 12:14:18 -0400

    Soluçao exercicio 3 - dia 8.4

---
+++ modulo-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-higher-order-functions-reduce/exercise3.js
@@ -0,0 +1,14 @@
+const books = require('./base');
+const assert = require('assert');
+
+// 3 - Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
+const expectedResult = 43;
+
+function averageAge() {
+  // escreva seu código aqui
+  const numberOfAuthors = books.length
+  const sumOfAges = books.reduce((accumulator, current) => accumulator + (current.releaseYear - current.author.birthYear), 0);
+  return sumOfAges / numberOfAuthors;
+}
+
+assert.strictEqual(averageAge(), expectedResult);
