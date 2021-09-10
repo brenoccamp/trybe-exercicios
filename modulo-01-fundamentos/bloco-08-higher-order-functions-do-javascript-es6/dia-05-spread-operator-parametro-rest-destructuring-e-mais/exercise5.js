723481d700cf866570797c99f31460496b1ab0d2
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-10 11:46:00 -0400

    Soluçao do exercicio 5 - dia 8.5

---
+++ modulo-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-spread-operator-parametro-rest-destructuring-e-mais/exercise5.js
@@ -0,0 +1,14 @@
+// 5 - Escreva a função swap , que dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando 1 linha só:
+// Dica: use array destructuring .
+const assert = require('assert');
+
+const myList = [1, 2, 3];
+
+// escreva swap abaixo
+const swap = ([a, b, c]) => [c, b, a];
+
+const swappedList = swap(myList);
+
+assert.strictEqual(swappedList[0], 3);
+assert.strictEqual(swappedList[1], 2);
+assert.strictEqual(swappedList[2], 1);
