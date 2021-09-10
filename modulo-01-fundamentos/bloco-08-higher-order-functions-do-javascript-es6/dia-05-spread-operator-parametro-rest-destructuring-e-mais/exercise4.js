4badf6926e6fd214467e760bedc100f81d5f52b3
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-10 11:41:46 -0400

    Soluçao do exercicio 4 - dia 8.5

---
+++ modulo-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-spread-operator-parametro-rest-destructuring-e-mais/exercise4.js
@@ -0,0 +1,40 @@
+// 4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+// Dica: use object destructuring .
+const assert = require('assert');
+const { SSL_OP_ALL } = require('constants');
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo
+const filterPeople = (array) => array.filter(({nationality, bornIn}) => nationality === 'Australian' && bornIn > 1900 && bornIn <= 2000); 
+
+const filteredPeople = filterPeople(people);
+
+assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
+assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });
