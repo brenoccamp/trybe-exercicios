1c1de54ce46727643f6fb965cfd19e800303a760
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-24 10:15:57 -0400

    Soluçao do exercicio 2 - dia 10.3

--- modulo-01-fundamentos/bloco-10-testes-automatizados-com-jest/dia-03-jest-simulando-comportamentos/exercise1.js
+++ modulo-01-fundamentos/bloco-10-testes-automatizados-com-jest/dia-03-jest-simulando-comportamentos/exercise1.js
@@ -5,3 +5,5 @@ function randomNumber() {
 };
 
 module.exports = { randomNumber };
+
+// 2 - Com a mesma função deste exercício, utilizando o mock, crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez. Faça os testes necessários.
