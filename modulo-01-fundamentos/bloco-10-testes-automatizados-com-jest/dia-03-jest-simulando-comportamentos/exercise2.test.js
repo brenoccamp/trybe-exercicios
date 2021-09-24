c6b42f882be2e5e4067c32cefe678c5912538ff3
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-24 10:22:17 -0400

    Exercicio 2 do dia 10.3 Refatorado

--- modulo-01-fundamentos/bloco-10-testes-automatizados-com-jest/dia-03-jest-simulando-comportamentos/exercise2.test.js
+++ modulo-01-fundamentos/bloco-10-testes-automatizados-com-jest/dia-03-jest-simulando-comportamentos/exercise2.test.js
@@ -3,12 +3,10 @@ const exercise1 = require('./exercise1');
 jest.mock('./exercise1');
 
 test('Testando nova implementação da função randomNumber', () => {
-  exercise1.randomNumber.mockImplementation((a, b) => a / b);
-  exercise1.randomNumber(10, 2);
-
+  exercise1.randomNumber.mockImplementationOnce((a, b) => a / b);
+  
+  expect(exercise1.randomNumber(10, 2)).toBe(5);
   expect(exercise1.randomNumber).toHaveBeenCalled();
   expect(exercise1.randomNumber).toHaveBeenCalledTimes(1);
   expect(exercise1.randomNumber).toHaveBeenCalledWith(10, 2);
-  expect(exercise1.randomNumber(8, 4)).toBe(2);
-  expect(exercise1.randomNumber).toHaveBeenCalledTimes(2);
 });
