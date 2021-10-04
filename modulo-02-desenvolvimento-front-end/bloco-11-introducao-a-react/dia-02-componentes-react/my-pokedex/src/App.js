f5a067472f590016eff743140c20b6b357ab6a7e
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-10-04 15:32:21 -0400

    Refatoraçao dos exercicios do dia 11.2 guiado pelo gabarito

--- modulo-02-desenvolvimento-front-end/bloco-11-introducao-a-react/dia-02-componentes-react/my-pokedex/src/App.js
+++ modulo-02-desenvolvimento-front-end/bloco-11-introducao-a-react/dia-02-componentes-react/my-pokedex/src/App.js
@@ -4,33 +4,33 @@ import pokemons from "./data";
 import Pokedex from "./Pokedex";
 
 // MINHA IMPLEMENTAÇÃO
-import Pokemon from "./Pokemon";
-class App extends Component {
-  render() {
-    return (
-      <Pokemon />
-    )
-  }
-}
-
+// import Pokemon from "./Pokemon";
 // class App extends Component {
 //   render() {
 //     return (
-//       <div className="App">
-//         <h1>Pokedex</h1>
-//         <Pokedex pokemons={pokemons} />
-//       </div>
-//     );
-//     // Neste arquivo, estamos renderizando o componente da Pokedex.
-//     // Criamos a prop "pokemons", e nela chamamos o nosso json "pokemons",
-//     // pokemons={} é a nossa prop e o valor dentro das {} é o nosso json,
-//     // ou seja nomeDaProps={valorDaProps}, o nome da props é o que foi passado
-//     // dentro do componente Pokedex.
-//     // O valor da props, vem o import que fizemos na linha 3 do arquivo data.js
-//     // pois desta forma poderemos trabalhar com esses dados via "props".
-//     // Uma informação importante é que podemos dar qualquer nome a uma prop,
-//     // mas atente-se para que seja um nome descritivo.
+//       <Pokemon />
+//     )
 //   }
 // }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Pokedex</h1>
+        <Pokedex pokemons={pokemons} />
+      </div>
+    );
+    // Neste arquivo, estamos renderizando o componente da Pokedex.
+    // Criamos a prop "pokemons", e nela chamamos o nosso json "pokemons",
+    // pokemons={} é a nossa prop e o valor dentro das {} é o nosso json,
+    // ou seja nomeDaProps={valorDaProps}, o nome da props é o que foi passado
+    // dentro do componente Pokedex.
+    // O valor da props, vem o import que fizemos na linha 3 do arquivo data.js
+    // pois desta forma poderemos trabalhar com esses dados via "props".
+    // Uma informação importante é que podemos dar qualquer nome a uma prop,
+    // mas atente-se para que seja um nome descritivo.
+  }
+}
 
 export default App;
--- modulo-02-desenvolvimento-front-end/bloco-11-introducao-a-react/dia-02-componentes-react/my-pokedex/src/Pokemon.js
+++ modulo-02-desenvolvimento-front-end/bloco-11-introducao-a-react/dia-02-componentes-react/my-pokedex/src/Pokemon.js
@@ -26,4 +26,40 @@ import PropTypes from 'prop-types';
 //   }
 // }
 
+
+//REFATORAÇÃO GUIADA PELO GABARITO
+class Pokemon extends Component {
+  render() {
+    const { pokemon: { name, type, averageWeight, image } } = this.props;
+    // Fizemos um map no nosso array de pokemons
+    // que vai renderizar o componente Pokemon para cada item do array.
+    // Então, recebemos a props "pokemon" que é um objeto do array de pokemons
+    // Nesses objetos temos informações como name, type e etc, então
+    // desestruturamos essas informações e renderizamos em uma tag HTML,
+    // no caso, utilizamos um <p>.
+    return (
+      <div className='pokemon'>
+        <div>
+          <p>{ name }</p>
+          <p>{ type }</p>
+          <p>{`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+        </div>
+        <img src={ image } alt={`${name} sprit`} />
+      </div>
+    );
+  }
+}
+
+Pokemon.proTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string,
+    type: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      measurementUnit: PropTypes.string,
+      value: PropTypes.number,
+    }),
+    image: PropTypes.string,
+  }).isRequired
+};
+
 export default Pokemon;
