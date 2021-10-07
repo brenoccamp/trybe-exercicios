8d5e6f3d5f3eb1c51960643a274e8f9541affaa0
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-10-06 23:21:20 -0400

    Soluçao dos exercicios de fixaçao Parte IIII - dia 11.1

--- modulo-02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ modulo-02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -63,6 +63,49 @@ import React from 'react';
 
 // }
 
+
+// EXERCÍCIOS DE FIXAÇAO PARTE III
+// class App extends React.Component {
+//   constructor() {
+//     super()
+//     this.handleClick1 = this.handleClick1.bind(this);
+//     this.handleClick2 = this.handleClick2.bind(this);
+//     this.handleClick3 = this.handleClick3.bind(this);
+//     this.state = {
+//       numeroDeCliques1: 0,
+//       numeroDeCliques2: 0,
+//       numeroDeCliques3: 0,
+//     }
+//   }
+
+//   handleClick1() {
+//     this.setState((estadoAnterior1, _props) => ({
+//       numeroDeCliques1: estadoAnterior1.numeroDeCliques1 + 1
+//     }))
+//   }
+//   handleClick2() {
+//     this.setState((estadoAnterior2, _props) => ({
+//       numeroDeCliques2: estadoAnterior2.numeroDeCliques2 + 1
+//     }))
+//   }
+//   handleClick3() {
+//     this.setState((estadoAnterior3, _props) => ({
+//       numeroDeCliques3: estadoAnterior3.numeroDeCliques3 + 1
+//     }))
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//       <button onClick={this.handleClick1}>{this.state.numeroDeCliques1}</button>
+//       <button onClick={this.handleClick2}>{this.state.numeroDeCliques2}</button>
+//       <button onClick={this.handleClick3}>{this.state.numeroDeCliques3}</button>
+//       </div>
+//     )
+//   }
+// }
+
+// EXERCÍCIOS DE FIXAÇAO PARTE IIII
 class App extends React.Component {
   constructor() {
     super()
@@ -76,16 +119,22 @@ class App extends React.Component {
     }
   }
 
+  setButtonColor(numOfClicks) { // Essa função não precisa do bind pois ela é usada apenas dentro do render() que está no contexto do componente App
+    return numOfClicks % 2 === 0 ? 'green' : 'white'
+  }
+
   handleClick1() {
-    this.setState((estadoAnterior1, _props) => ({
-      numeroDeCliques1: estadoAnterior1.numeroDeCliques1 + 1
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques1: estadoAnterior.numeroDeCliques1 + 1
     }))
   }
+
   handleClick2() {
-    this.setState((estadoAnterior2, _props) => ({
-      numeroDeCliques2: estadoAnterior2.numeroDeCliques2 + 1
+    this.setState(({ numeroDeCliques2 }) => ({
+      numeroDeCliques2: numeroDeCliques2 + 1
     }))
   }
+
   handleClick3() {
     this.setState((estadoAnterior3, _props) => ({
       numeroDeCliques3: estadoAnterior3.numeroDeCliques3 + 1
@@ -93,15 +142,15 @@ class App extends React.Component {
   }
 
   render() {
+    const { numeroDeCliques1, numeroDeCliques2, numeroDeCliques3 } = this.state;
     return (
       <div>
-      <button onClick={this.handleClick1}>{this.state.numeroDeCliques1}</button>
-      <button onClick={this.handleClick2}>{this.state.numeroDeCliques2}</button>
-      <button onClick={this.handleClick3}>{this.state.numeroDeCliques3}</button>
+      <button onClick={this.handleClick1} style={{ backgroundColor: this.setButtonColor(numeroDeCliques1) }}>{numeroDeCliques1}</button>
+      <button onClick={this.handleClick2} style={{ backgroundColor: this.setButtonColor(numeroDeCliques2)}}>{numeroDeCliques2}</button>
+      <button onClick={this.handleClick3} style={{ backgroundColor: this.setButtonColor(numeroDeCliques3)}}>{numeroDeCliques3}</button>
       </div>
     )
   }
-
 }
 
 export default App;
