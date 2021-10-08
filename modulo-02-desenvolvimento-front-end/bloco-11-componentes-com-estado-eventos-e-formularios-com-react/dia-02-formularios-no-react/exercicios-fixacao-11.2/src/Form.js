7d8254b6921dd57159d4167f7a7f5660b73cc5a3
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-10-08 11:29:47 -0400

    Soluçao dos exercicios de fixaçao Parte II - Dia 11.2

--- modulo-02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/exercicios-fixacao-11.2/src/Form.js
+++ modulo-02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/exercicios-fixacao-11.2/src/Form.js
@@ -1,51 +1,81 @@
 import React from "react";
+import './Form.css';
 
 class Form extends React.Component {
   constructor() {
-    super()
-    this.selectedSkill = this.selectedSkill.bind(this);
-    this.markedCheckbox = this.markedCheckbox.bind(this);
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.fileInput = React.createRef();
     this.state = {
-      skills: '',
-      checkbox: '',
-      email: '',
-      textarea: '',
-    }
+      textarea: "Escreva aqui seu texto!",
+    };
   }
 
-  selectedSkill(event) {
-    this.setState(({ skills: event.target.value }))
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    this.setState({ [name]: value });
   }
 
-  markedCheckbox(event) {
-    this.setState(({ checkbox: event.target.value }))
-  }
+  submit = (event) => {
+    event.preventDefault();
+    const filesInput = this.fileInput.current.files[0] === undefined ? alert('Faça Upload de um arquivo!') : alert(`Você adicionou o arquivo ${this.fileInput.current.files[0].name}`);
+  };
 
   render() {
     return (
       <div>
-        <h1>Exercício de fixação Parte I - Dia 11.2 - Formulário para testar as habilidades</h1>
-        <form className="form">
-          <label for='skills'>Escolha uma habilidade:
-            <select name="skills" id="skills" onChange={this.selectedSkill}>
-              <option value="html5">HTML5</option>
-              <option value="js">JavaScript</option>
-              <option value="css">CSS</option>
-              <option value="react">React</option>
-            </select>
-          </label>
-          <label for="checkbox">Marque a caixa:
-            <input name="checkbox" type="checkbox" onChange={this.markedCheckbox}></input>
-          </label>
-          <label for="email">Digite seu email:
-            <input type="email" name="email"></input>
-          </label>
-          <label for="textarea">Digite algo
-          <textarea id='textarea' name='textarea' row='5' cols='30'></textarea>
-          </label>
+        <h1>
+          Exercício de fixação Parte I - Dia 11.2 - Formulário para testar as
+          habilidades
+        </h1>
+        <form className="form" onSubmit={this.submit}>
+          <fieldset className='fieldset'>
+            <legend>Preencha o formulário abaixo:</legend>
+            <label for="skills">
+              Escolha uma habilidade:
+              <select name="skills" id="skills" onChange={this.handleChange}>
+                <option value="html5">HTML5</option>
+                <option value="js">JavaScript</option>
+                <option value="css">CSS</option>
+                <option value="react">React</option>
+              </select>
+            </label>
+            <label for="checkbox">
+              Marque a caixa:
+              <input
+                name="checkbox"
+                type="checkbox"
+                onChange={this.handleChange}
+              ></input>
+            </label>
+            <label for="email">
+              Digite seu email:
+              <input
+                type="email"
+                name="email"
+                onChange={this.handleChange}
+              ></input>
+            </label>
+            <label for="textarea">
+              Digite algo
+              <textarea
+                id="textarea"
+                name="textarea"
+                row="5"
+                cols="30"
+                onChange={this.handleChange}
+              ></textarea>
+            </label>
+            <label>
+              Upload file:
+              <input type="file" ref={this.fileInput} />
+            </label>
+            <button type="submit">Submit</button>
+          </fieldset>
         </form>
       </div>
-    )
+    );
   }
 }
 
