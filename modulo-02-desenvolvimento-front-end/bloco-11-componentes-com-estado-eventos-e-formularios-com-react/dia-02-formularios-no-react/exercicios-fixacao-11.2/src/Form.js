567261ee990d28841467d92fe38a7138f0d0e8bb
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-10-07 22:40:28 -0400

    Soluçao do exercicio de fixaçao Parte I - dia 11.2

---
+++ modulo-02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/exercicios-fixacao-11.2/src/Form.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+class Form extends React.Component {
+  constructor() {
+    super()
+    this.selectedSkill = this.selectedSkill.bind(this);
+    this.state = {
+      skills: '',
+      checkbox: false,
+      email: '',
+      textarea: '',
+    }
+  }
+
+  selectedSkill(event) {
+    this.setState(({ skills: event.target.value }))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Exercício de fixação Parte I - Dia 11.2 - Formulário para testar as habilidades</h1>
+        <form className="form">
+          <label for='skills'>Escolha uma habilidade:
+            <select name="skills" id="skills" onChange={this.selectedSkill}>
+              <option value="html5">HTML5</option>
+              <option value="js">JavaScript</option>
+              <option value="css">CSS</option>
+              <option value="react">React</option>
+            </select>
+          </label>
+          <label for="checkbox">Marque a caixa:
+            <input name="checkbox" type="checkbox"></input>
+          </label>
+          <label for="email">Digite seu email:
+            <input type="email" name="email"></input>
+          </label>
+          <label for="textarea">Digite algo
+          <textarea id='textarea' name='textarea' row='5' cols='30'></textarea>
+          </label>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Form;
