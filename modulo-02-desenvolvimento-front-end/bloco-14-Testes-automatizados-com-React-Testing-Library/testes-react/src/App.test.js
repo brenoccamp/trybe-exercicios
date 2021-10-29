6dbfda7af411bc70f5320cb67dd81f77c7d6797f
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-10-29 00:09:44 -0400

    Cria app testes-react

---
+++ modulo-02-desenvolvimento-front-end/bloco-14-Testes-automatizados-com-React-Testing-Library/testes-react/src/App.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+
+test('Verificando se existe o campo Email.', () => {
+  render(<App />);
+  const inputEmail = screen.getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveProperty('type', 'email');
+});
+
+test('Verificando se existem dois botões', () => {
+  render(<App />);
+  const buttons = screen.getAllByRole('button');
+  expect(buttons).toHaveLength(2);
+});
+
+test('Verificando se existe um botão de enviar', () => {
+  render(<App />);
+  const btnSend = screen.getByTestId('id-send');
+  expect(btnSend).toBeInTheDocument();
+  expect(btnSend).toHaveProperty('type', 'button');
+  expect(btnSend).toHaveValue('Enviar');
+});
+
+test('Verificando se o botão e o campo email estão funcionando.', () => {
+  render(<App />);
+
+  const EMAIL_USER = 'email@email.com';
+
+  const textEmail = screen.getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor:');
+
+  const btnSend = screen.getByTestId('id-send');
+  const inputEmail = screen.getByLabelText('Email');
+  userEvent.type(inputEmail, EMAIL_USER);
+  userEvent.click(btnSend);
+
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Valor: ${ EMAIL_USER }`);
+});
