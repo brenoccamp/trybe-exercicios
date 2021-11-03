eb417c0916c28f42bd32acedf574ed20e0c703e0
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-11-03 10:22:42 -0400

    fixacao 14.2

--- modulo-02-desenvolvimento-front-end/bloco-14-Testes-automatizados-com-React-Testing-Library/dia-02-RTL-mocks-e-inputs/fixacao-dia-14.2/src/App.js
+++ modulo-02-desenvolvimento-front-end/bloco-14-Testes-automatizados-com-React-Testing-Library/dia-02-RTL-mocks-e-inputs/fixacao-dia-14.2/src/App.js
@@ -1,31 +1,52 @@
-// App.js
 import React from 'react';
-import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      joke: '',
+      nome: '',
+      email: '',
     };
   }
 
-  componentDidMount() {
-    const API_URL = 'https://icanhazdadjoke.com/';
-    fetch(API_URL, { headers: { Accept: 'application/json' } })
-      .then((response) => response.json())
-      .then((data) => this.setState({ joke: data.joke }));
+  handleInput(e) {
+    const { name, value } = e.target;
+    console.log(name, value);
+    this.setState({ [name]: value });
   }
 
   render() {
-    const { joke } = this.state;
+    const { nome, email } = this.state;
 
     return (
-      <div className="App">
-        {joke}
+      <div>
+        <h1>Teste de inputs</h1>
+        <p>
+          <label htmlFor="nome">
+            Nome
+            <input
+              type="text"
+              id="nome"
+              name="nome"
+              value={ nome }
+              onChange={ (e) => this.handleInput(e) }
+            />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={ email }
+              onChange={ (e) => this.handleInput(e) }
+            />
+          </label>
+        </p>
       </div>
     );
   }
 }
-
 export default App;
--- modulo-02-desenvolvimento-front-end/bloco-14-Testes-automatizados-com-React-Testing-Library/dia-02-RTL-mocks-e-inputs/fixacao-dia-14.2/src/App.test.js
+++ modulo-02-desenvolvimento-front-end/bloco-14-Testes-automatizados-com-React-Testing-Library/dia-02-RTL-mocks-e-inputs/fixacao-dia-14.2/src/App.test.js
@@ -1,8 +1,20 @@
+// App.test.js
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('alterando o valor dos campos e testando o valor guardado', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputNome = screen.getByRole('textbox', { name: /nome/i });
+  expect(inputNome).toBeInTheDocument();
+  expect(inputNome).toHaveValue('');
+  userEvent.type(inputNome, 'Estudante da Trybe');
+  expect(inputNome).toHaveValue('Estudante da Trybe');
+
+  const inputEmail = screen.getByRole('textbox', { name: /email/i });
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveValue('');
+  userEvent.type(inputEmail, 'estudante@trybe.com');
+  expect(inputEmail).toHaveValue('estudante@trybe.com');
 });
