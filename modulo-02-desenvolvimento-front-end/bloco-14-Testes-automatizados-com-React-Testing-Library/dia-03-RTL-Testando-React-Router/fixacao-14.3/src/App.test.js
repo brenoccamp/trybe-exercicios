985f83f91a23c99b5fa7ce8a7f38a9eed8a316ce
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-11-04 10:13:06 -0400

    exemplo de teste com rotas e funçao renderWithRouter

--- modulo-02-desenvolvimento-front-end/bloco-14-Testes-automatizados-com-React-Testing-Library/dia-03-RTL-Testando-React-Router/fixacao-14.3/src/App.test.js
+++ modulo-02-desenvolvimento-front-end/bloco-14-Testes-automatizados-com-React-Testing-Library/dia-03-RTL-Testando-React-Router/fixacao-14.3/src/App.test.js
@@ -1,8 +1,42 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('teste da aplicação toda', () => {
+  it('deve renderizar o componente App', () => {
+    renderWithRouter(<App />);
+
+    const homeTitle = screen.getByRole('heading', {
+      name: 'Você está na página Início',
+    });
+    expect(homeTitle).toBeInTheDocument();
+  });
+
+  it('deve renderizar o componente Sobre', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: 'Sobre' });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+
+    const aboutTitle = screen.getByRole('heading',
+      { name: 'Você está na página Sobre' });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  it('deve testar um caminho não existente e a renderização do Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pagina/que-nao-existe/');
+
+    const notFoundTitle = screen.getByRole('heading',
+      { name: 'Página não encontrada' });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
 });
