c8bb5372ac5984f8c91c2d5072133cb69dfd9334
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-06 09:50:06 -0400

    Exercise 2 and 3 done!

--- modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-01-intro-nodeJS-um-motor-javascript/dayExercises/imc.js
+++ modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-01-intro-nodeJS-um-motor-javascript/dayExercises/imc.js
@@ -1,8 +1,9 @@
-const weight = 80;
-const height = 175;
+const readline = require('readline-sync');
+const weight = readline.questionInt('May I have your weight?');
+const height = readline.questionInt('May I have your height?');
 
 const calcImc = () => {
-  console.log(`Your weight is ${weight} and your height is ${height}cm`);
+  console.log(`Your weight is ${weight}kg and your height is ${height}cm`);
 
   const imc = (weight / Math.pow(height/100, 2)).toFixed(2);
 
