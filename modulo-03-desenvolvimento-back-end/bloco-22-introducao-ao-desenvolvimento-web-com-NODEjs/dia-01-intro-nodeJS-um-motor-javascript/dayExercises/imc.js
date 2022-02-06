8499d703affc7f8d4ceaa0d1c9a1fa5a7a9ad7d0
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-06 10:37:58 -0400

    Exercises 4 and 5 done!

--- modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-01-intro-nodeJS-um-motor-javascript/dayExercises/imc.js
+++ modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-01-intro-nodeJS-um-motor-javascript/dayExercises/imc.js
@@ -1,5 +1,5 @@
 const readline = require('readline-sync');
-const weight = readline.questionInt('May I have your weight?');
+const weight = readline.questionFloat('May I have your weight?');
 const height = readline.questionInt('May I have your height?');
 
 const calcImc = () => {
@@ -7,7 +7,26 @@ const calcImc = () => {
 
   const imc = (weight / Math.pow(height/100, 2)).toFixed(2);
 
-  console.log(`Your IMC: ${imc}`);
+  console.log(`Your IMC is: ${imc}`);
+
+  if(imc <= 18.5) {
+    return console.log('Category: Under weight!');
+  }
+  if (imc <= 24.9) {
+    return console.log('Category: Normal weight!');
+    }
+  if (imc <= 29.9) {
+    return console.log('Category: Overweight!');
+  }
+  if (imc <= 34.9) {
+    return console.log('Category: Obesity grade I!');
+  }
+  if (imc <= 39.9) {
+    return console.log('Category: Obesity grade II!');
+  }
+  if (imc >= 40) {
+    return console.log('Category: Obesity grade III and IV!');
+  }
 }
 
 calcImc();
