8a0deff2b5f7b26ad14f32f59393e28cf2098f19
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-06 11:41:14 -0400

    Exercise 7 done!

--- modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-01-intro-nodeJS-um-motor-javascript/dayExercises/sorteio.js
+++ modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-01-intro-nodeJS-um-motor-javascript/dayExercises/sorteio.js
@@ -1,4 +1,25 @@
 const readline = require('readline-sync');
 
-// const 
-console.log('testing prize draw script');
+
+const prizeDrawFeedback = () => {
+  const aleatoryNumber = Math.floor(Math.random() * 11);
+
+  const numberInputedByUser = readline.questionInt("Tell me an integer between 0 and 10 to know if it's the number I'm thinking:");
+
+  if (Number(numberInputedByUser) === aleatoryNumber) return console.log(aleatoryNumber, 'Congratulations, right number!');
+  if (Number(numberInputedByUser) !== aleatoryNumber) return console.log(`Oops, not this time. The number was ${aleatoryNumber}`);
+}
+
+const runGame = () => {
+  prizeDrawFeedback();
+
+  const playAgain = readline.question('Do you want to play again? Type "y" for yes or anything else for no:')
+  console.log(playAgain);
+  if (playAgain !== 'y' && playAgain !== 'Y') {
+    return console.log('Ok, until next time!');
+  }
+
+  runGame();
+}
+
+runGame();
