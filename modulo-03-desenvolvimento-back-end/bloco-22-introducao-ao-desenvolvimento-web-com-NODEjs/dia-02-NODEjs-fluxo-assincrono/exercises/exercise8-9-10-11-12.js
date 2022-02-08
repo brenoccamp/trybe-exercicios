364421ea962b7822cae735256bc20a42799298a2
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-08 14:56:01 -0400

    Exercises 10, 11 and 12 done!

--- modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-02-NODEjs-fluxo-assincrono/exercises/exercise8-9-10-11-12.js
+++ modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-02-NODEjs-fluxo-assincrono/exercises/exercise8-9-10-11-12.js
@@ -1,6 +1,9 @@
 function fizzBuzz(number) {
   return new Promise((resolve, reject) => {
     if (number % 3 === 0 && number % 5 === 0) return resolve('FizzBuzz');
+    if (number % 3 === 0) return resolve('Fizz');
+    if (number % 5 === 0) return resolve ('Buzz');
 
+    reject(number);
   });
 }
