08631fad983acd70c0bdf9688392bd66bed98d75
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-09 15:11:40 -0400

    Exercise 7 done!

---
+++ modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-04-express-http-com-nodeJS/exercises/exercise7.js
@@ -0,0 +1,22 @@
+// Create one endpoint GET called /simpsons/:id that returns a character searched by id
+const appRequires = require('./helper');
+const { getSimpsons } = require('./exercise5');
+
+const app = appRequires();
+
+function exercise7() {
+  app.get('/simpsons/:id', async (req, res) => {
+    const simpsons = await getSimpsons();
+    const { id } = req.params;
+
+    const searchedSimpson = simpsons.find((simpson) => parseInt(simpson.id) === parseInt(id));
+
+    if (!searchedSimpson) return res.status(404).json({ message: `Simpson with id:${id} not found!` });
+
+    res.status(200).json(searchedSimpson);
+  });
+
+  app.listen('3000', () => console.log('App running on port 3000'));
+}
+
+module.exports = exercise7;
--- modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-04-express-http-com-nodeJS/exercises/index.js
+++ modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-04-express-http-com-nodeJS/exercises/index.js
@@ -3,9 +3,11 @@ const exercise2 = require('./exercise2');
 const exercise3 = require('./exercise3');
 const exercise4 = require('./exercise4');
 const exercise6 = require('./exercise6');
+const exercise7 = require('./exercise7');
 
 // exercise1();
 // exercise2();
 // exercise3();
 // exercise4();
-exercise6();
+// exercise6();
+exercise7();
