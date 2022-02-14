95d8364eceded3b8b29ab12f9419f1a72ea0e0b7
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-14 14:19:52 -0400

    Testing request without bodyParser.json(). The body is undefined

--- modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-05-express-middlewares/fixationExercises/server.js
+++ modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-05-express-middlewares/fixationExercises/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 app.post('/hello', (req, res) => {
   // req.body agora está disponível
