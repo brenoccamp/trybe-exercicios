943329f5403153f25f0abdb2be2c011a69f25c6f
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-14 20:53:37 -0400

    Activity 2 done!

--- modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-05-express-middlewares/dayExercises/controllers/btc.js
+++ modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-NODEjs/dia-05-express-middlewares/dayExercises/controllers/btc.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const tokenValidation = require('../middlewares/tokenValidation');
+const axios = require('axios');
+const cors = require('cors');
 
 const router = express.Router();
+// router.use(cors());
+// router.use(express.json());
+
+const ENDPOINTCOINDDESK = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json)'
 
 router.get(
   '/price',
+  tokenValidation,
+  async (_req, res, _next) => {
+    const apiResponse = await axios.get(ENDPOINTCOINDDESK);
 
+    res.status(200).json(apiResponse.data);
+  }
 )
 
 module.exports = router;
