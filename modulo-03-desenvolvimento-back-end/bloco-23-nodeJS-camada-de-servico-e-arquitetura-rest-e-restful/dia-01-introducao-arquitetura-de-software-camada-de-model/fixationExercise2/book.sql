9f998f7cf5cdde73f86cfed2558551c6df6936ed
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-17 10:56:16 -0400

    Create book.sql to add book table on mysql db

---
+++ modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-01-introducao-arquitetura-de-software-camada-de-model/fixationExercise2/book.sql
@@ -0,0 +1,18 @@
+
+    CREATE TABLE books (
+        id INT NOT NULL AUTO_INCREMENT,
+        title VARCHAR(90) NOT NULL,
+        author_id INT(11) NOT NULL,
+        PRIMARY KEY(id),
+        FOREIGN KEY (author_id) REFERENCES authors (id)
+    );
+
+    INSERT INTO books (title, author_id)
+    VALUES
+        ('A Game of Thrones', 1),
+        ('A Clash of Kings', 1),
+        ('A Storm of Swords', 1),
+        ('The Lord of The Rings - The Fellowship of the Ring', 2),
+        ('The Lord of The Rings - The Two Towers', 2),
+        ('The Lord of The Rings - The Return of The King', 2),
+        ('Foundation', 3);
