10594d25e970ea77f9863b568fb532b0eba44b29
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-17 09:49:58 -0400

    Create db to make fixation exercise

---
+++ modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-01-introducao-arquitetura-de-software-camada-de-model/model_example_fixation.sql
@@ -0,0 +1,22 @@
+    CREATE DATABASE IF NOT EXISTS model_example;
+
+    USE model_example;
+
+    CREATE TABLE authors
+    (
+        id INT NOT NULL AUTO_INCREMENT,
+        first_name VARCHAR(30) NOT NULL,
+        middle_name VARCHAR(30),
+        last_name VARCHAR(30) NOT NULL,
+        birthday DATE,
+        nationality VARCHAR(100),
+        PRIMARY KEY(id)
+    );
+
+    INSERT INTO authors (first_name,middle_name,last_name,birthday,nationality)
+
+    VALUES ('George','R. R.','Martin','1948-09-20','norte-americano'),
+        ('J.','R. R.','Tolkien','1892-01-03','britânico'),
+        ('Isaac',NULL,'Asimov','1920-01-20','russo-americano'),
+        ('Frank',NULL,'Herbert','1920-02-11','norte-americano'),
+        ('Júlio',NULL,'Verne','1905-03-24','francês');
