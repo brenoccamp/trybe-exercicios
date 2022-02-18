646627659ed7567dc776843574c009afb4f0c06f
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-18 14:16:55 -0400

    Create Author model

---
+++ modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/fixationExercises/hello-msc/models/Author.js
@@ -0,0 +1,73 @@
+const connection = require('./connection');
+
+// Cria uma string com o nome completo da pessoa autora
+
+const getNewAuthor = (authorData) => {
+const { id, firstName, middleName, lastName } = authorData;
+
+const fullName = [firstName, middleName, lastName]
+  .filter((name) => name)
+  .join(' ');
+
+return {
+  id,
+  firstName,
+  middleName,
+  lastName,
+  name: fullName,
+ };
+};
+
+// Converte o nome dos campos de snake_case para camelCase
+const serialize = (authorData) => authorData.map((item) => getNewAuthor({
+  id: item.id,
+  firstName: item.first_name,
+  middleName: item.middle_name,
+  lastName: item.last_name}));
+
+// Busca todos os autores do banco.
+
+const getAll = async () => {
+  const [authors] = await connection.execute(
+    'SELECT id, first_name, middle_name, last_name FROM model_example.authors;'
+  );
+  return serialize(authors);
+};
+
+/*
+Busca uma pessoa autora específica, a partir do seu ID
+@param {String} id ID da pessoa autora a ser recuperado
+*/
+const findById = async (id) => {
+  const query =
+    'SELECT id, first_name, middle_name, last_name FROM model_example.authors WHERE id = ?';
+
+  const [authorData] = await connection.execute(query, [id]);
+
+  if (authorData.length === 0) return null;
+
+  return serialize(authorData)[0];
+};
+
+const isValid = (firstName, middleName, lastName) => {
+    if (!firstName || typeof firstName !== 'string') return false;
+    if (!lastName || typeof lastName !== 'string') return false;
+    if (middleName && typeof middleName !== 'string') return false;
+
+    return true;
+};
+
+const createAuthor = async (firstName, middleName, lastName) => {
+  const [author] = await connection.execute(
+    'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?, ?, ?)',
+    [firstName, middleName, lastName]
+  );
+  return getNewAuthor({ id: author.insertId, firstName, middleName, lastName });
+}
+
+module.exports = {
+  getAll,
+  findById,
+  isValid,
+  createAuthor,
+};
