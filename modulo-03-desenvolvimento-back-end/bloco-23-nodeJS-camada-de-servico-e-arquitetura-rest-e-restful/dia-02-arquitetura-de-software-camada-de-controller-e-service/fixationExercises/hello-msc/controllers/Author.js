58c49f7dbd71d389cdc91a0978fbd6d589ed2a0c
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-18 15:09:52 -0400

    index.js refatored. Now the middlewares are in directory controllers/Author.js

---
+++ modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/fixationExercises/hello-msc/controllers/Author.js
@@ -0,0 +1,34 @@
+const Author = require('../services/Authors');
+
+const getAll = async (_req, res) => {
+  const authors = await Author.getAll();
+
+  res.status(200).json(authors);
+};
+
+const findById = async (req, res) => {
+  const { id } = req.params;
+
+  const author = await Author.findById(id);
+
+  if (!author) return res.status(404).json({ message: 'Not found' });
+
+  res.status(200).json(author);
+};
+
+const createAuthor = async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  const author = await Author.createAuthor(first_name, middle_name, last_name);
+
+  if (!author) return res.status(400).json({ message: 'Dados inválidos' });
+
+  res.status(201).json(author);
+};
+
+
+module.exports = {
+  getAll,
+  findById,
+  createAuthor,
+}
--- modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/fixationExercises/hello-msc/index.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/fixationExercises/hello-msc/index.js
@@ -1,37 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const Author = require('./services/Authors');
+const Author = require('./controllers/Author');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/authors', async (_req, res) => {
-  const authors = await Author.getAll();
+app.get('/authors', Author.getAll);
 
-  res.status(200).json(authors);
-});
+app.get('/authors/:id', Author.findById);
 
-app.get('/authors/:id', async (req, res) => {
-  const { id } = req.params;
-
-  const author = await Author.findById(id);
-
-  if (!author) return res.status(404).json({ message: 'Not found' });
-
-  res.status(200).json(author);
-});
-
-app.post('/authors', async (req, res) => {
-  const { first_name, middle_name, last_name } = req.body;
-
-  const author = await Author.createAuthor(first_name, middle_name, last_name);
-
-  if (!author) return res.status(400).json({ message: 'Dados inválidos' });
-
-  res.status(201).json(author);
-});
+app.post('/authors', Author.createAuthor);
 
 const PORT = 3000;
 
