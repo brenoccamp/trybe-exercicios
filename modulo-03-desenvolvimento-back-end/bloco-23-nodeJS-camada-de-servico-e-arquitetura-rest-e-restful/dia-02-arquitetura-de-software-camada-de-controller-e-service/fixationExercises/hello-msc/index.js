631f3540efab56b9a854a204299a072d181e5f1f
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-18 14:11:59 -0400

    Create index.js with the API endpoints

---
+++ modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/fixationExercises/hello-msc/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const Author = require('./models/Author');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/authors', async (_req, res) => {
+  const authors = await Author.getAll();
+
+  res.status(200).json(authors);
+});
+
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const author = await Author.findById(id);
+
+  if (!author) return res.status(404).json({ message: 'Not found' });
+
+  res.status(200).json(author);
+});
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+    return res.status(400).json({ message: 'Dados inválidos' });
+  }
+
+  await Author.createAuthor(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'Pessoa autora criada com sucesso! ' });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
+});
