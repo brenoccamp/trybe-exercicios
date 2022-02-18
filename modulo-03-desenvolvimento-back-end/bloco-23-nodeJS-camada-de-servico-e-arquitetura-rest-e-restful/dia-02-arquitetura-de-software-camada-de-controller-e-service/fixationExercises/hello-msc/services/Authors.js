22ae19f593bf4574997667dc262768762319ae8f
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-18 14:52:48 -0400

    Create the service to validate firstName, middleName and lastName. Taking it off from Author model

--- modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/fixationExercises/hello-msc/services/Authors.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/fixationExercises/hello-msc/services/Authors.js
@@ -2,6 +2,7 @@
 
 const Author = require('../models/Authors');
 
+// Cria uma string com o nome completo da pessoa autora
 const getNewAuthor = (authorData) => {
   const { id, firstName, middleName, lastName } = authorData;
 
@@ -26,12 +27,17 @@ const isValid = (firstName, middleName, lastName) => {
   return true;
   }
 
+// Busca todos os autores do banco.
 const getAll = async () => {
   const authors = await Author.getAll();
 
   return authors.map(getNewAuthor);
 }
 
+/*
+Busca uma pessoa autora específica, a partir do seu ID
+@param {String} id ID da pessoa autora a ser recuperado
+*/
 const findById = async (id) => {
   const author = await Author.findById(id);
 
@@ -40,8 +46,18 @@ const findById = async (id) => {
   return getNewAuthor(author);
 };
 
+const createAuthor = async (firstName, middleName, lastName) => {
+  const validAuthor = isValid(firstName, middleName, lastName);
+
+  if (!validAuthor) return false;
+
+  await Author.createAuthor(firstName, middleName, lastName);
+
+  return true;
+};
+
 module.exports = {
   getAll,
   findById,
-  isValid
+  createAuthor,
 }
