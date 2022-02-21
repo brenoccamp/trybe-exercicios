f81b32f6c1f7de01ca9ca11c582c543aea4183d0
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-21 14:55:30 -0400

    Standardize res to json() and fix all status responses

--- modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/dayExercises/controllers/productController.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/dayExercises/controllers/productController.js
@@ -6,13 +6,13 @@ const router = express.Router();
 router.get('/', async (_req, res, _next) => {
   const products = await ProductModel.getAll();
 
-  res.send(products);
+  res.status(200).json(products);
 });
 
 router.get('/:id', async (req, res, _next) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.send(product);
+  res.status(200).json(product);
 });
 
 router.post('/', async (req, res) => {
@@ -20,13 +20,13 @@ router.post('/', async (req, res) => {
 
   const newProduct = await ProductModel.add(name, brand);
 
-  res.send(newProduct);
+  res.status(201).json(newProduct);
 });
 
 router.delete('/:id', async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
 
-  res.send(products);
+  res.status(200).json(products);
 });
 
 router.put('/:id', async (req, res) => {
@@ -34,7 +34,7 @@ router.put('/:id', async (req, res) => {
 
   const products = await ProductModel.update(req.params.id, name, brand);
 
-  res.send(products);
+  res.status(200).json(products);
 });
 
 module.exports = router;
