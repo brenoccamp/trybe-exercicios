dd034d2841a568b1f82ba8faf48108b38a1ad19c
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-21 14:52:48 -0400

    Standardize endpoints

--- modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/dayExercises/controllers/productController.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/dayExercises/controllers/productController.js
@@ -9,13 +9,13 @@ router.get('/', async (_req, res, _next) => {
   res.send(products);
 });
 
-router.get('/get-by-id/:id', async (req, res, _next) => {
+router.get('/:id', async (req, res, _next) => {
   const product = await ProductModel.getById(req.params.id);
 
   res.send(product);
 });
 
-router.post('/add-user', async (req, res) => {
+router.post('/', async (req, res) => {
   const { name, brand } = req.body;
 
   const newProduct = await ProductModel.add(name, brand);
@@ -23,13 +23,13 @@ router.post('/add-user', async (req, res) => {
   res.send(newProduct);
 });
 
-router.delete('/delete-user/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
 
   res.send(products);
 });
 
-router.put('/update-user/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
   const { name, brand } = req.body;
 
   const products = await ProductModel.update(req.params.id, name, brand);
