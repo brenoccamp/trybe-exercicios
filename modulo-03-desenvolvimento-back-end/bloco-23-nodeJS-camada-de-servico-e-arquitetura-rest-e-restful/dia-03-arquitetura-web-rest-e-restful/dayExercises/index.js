fa19c2f371589351570682f912aab87c4b1e7a1a
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-21 14:39:37 -0400

    Install body-parser and create index.js with code that we will need

---
+++ modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/dayExercises/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/products', require('./controllers/productController'));
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000!");
+});
