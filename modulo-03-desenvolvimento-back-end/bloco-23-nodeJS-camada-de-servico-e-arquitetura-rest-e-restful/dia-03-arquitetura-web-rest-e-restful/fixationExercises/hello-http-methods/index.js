46fe241007e694743618c17c8241bb8fcfc4fb95
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-21 14:20:35 -0400

    Create body

--- modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/fixationExercises/hello-http-methods/index.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/fixationExercises/hello-http-methods/index.js
@@ -6,13 +6,23 @@ const API_TOKEN = '2d635ea9b637ea0f27d58985cc161d64';
 
 // Criamos um novo objeto de Headers
 const headers = new fetch.Headers({
-  Authorization: API_TOKEN});
+  Authorization: API_TOKEN,
+  // Precisamos adicionar o header `Content-Type` e defini-lo como `application/json`
+  'Content-Type': 'application/json',});
+
+// Depois, criamos o body
+// Utilizamos o `stringify` para que possamos enviar esse body como JSON
+const body = JSON.stringify({
+  name: 'Tryber',
+  email: 'tryber@betrybe.com',
+  password: 'Tr1b3r'});
 
 // Para aquecer, vamos começar com uma requisição do tipo `GET`
 fetch('https://postman-echo.com/post?param1=teste', {
   // Passamos o objeto de headers como parâmetro para o fetch
   headers,
   method: 'POST',
+  body,
 })
   .then((response) => {
     // Ao receber a resposta, verificamos se correu tudo bem
