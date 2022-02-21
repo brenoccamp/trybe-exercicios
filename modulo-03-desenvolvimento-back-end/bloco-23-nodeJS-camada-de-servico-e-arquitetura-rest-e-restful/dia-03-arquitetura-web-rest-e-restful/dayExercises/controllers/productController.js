ba67c59f9f9292f74ea67ad92472d589fd7277fc
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-21 15:00:25 -0400

    Add try catch at some endpoints to take all errors

--- modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/dayExercises/controllers/productController.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/dayExercises/controllers/productController.js
@@ -12,29 +12,44 @@ router.get('/', async (_req, res, _next) => {
 router.get('/:id', async (req, res, _next) => {
   const product = await ProductModel.getById(req.params.id);
 
+  if (product === null) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+
   res.status(200).json(product);
 });
 
 router.post('/', async (req, res) => {
   const { name, brand } = req.body;
+  try {
+    const newProduct = await ProductModel.add(name, brand);
 
-  const newProduct = await ProductModel.add(name, brand);
+    return res.status(201).json(newProduct);
+  } catch (e) {
+    return res.status(500).json({ message: 'Something was wrong' });
+  }
 
-  res.status(201).json(newProduct);
 });
 
 router.delete('/:id', async (req, res) => {
-  const products = await ProductModel.exclude(req.params.id);
+  try {
+    const products = await ProductModel.exclude(req.params.id);
 
-  res.status(200).json(products);
+    return res.status(200).json(products);
+  } catch (e) {
+    return res.status(500).json({ message: 'Something was wrong' });
+  }
 });
 
 router.put('/:id', async (req, res) => {
   const { name, brand } = req.body;
+  try {
+    const products = await ProductModel.update(req.params.id, name, brand);
 
-  const products = await ProductModel.update(req.params.id, name, brand);
-
-  res.status(200).json(products);
+    return res.status(200).json(products);
+  } catch (e) {
+    return res.status(500).json({ message: 'Something was wrong' });
+  }
 });
 
 module.exports = router;
