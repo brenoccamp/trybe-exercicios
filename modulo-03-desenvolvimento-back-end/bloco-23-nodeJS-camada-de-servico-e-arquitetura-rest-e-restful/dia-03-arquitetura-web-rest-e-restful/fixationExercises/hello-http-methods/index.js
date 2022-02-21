38eb5469cbfa3c1d5d18075a986daea0dfe5e793
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-21 14:00:03 -0400

    Create index.js

---
+++ modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/fixationExercises/hello-http-methods/index.js
@@ -0,0 +1,29 @@
+const fetch = require('node-fetch');
+
+// Para aquecer, vamos começar com uma requisição do tipo `GET`
+fetch('https://postman-echo.com/get?param1=teste')
+  .then((response) => {
+    // Ao receber a resposta, verificamos se correu tudo bem
+    if (!response.ok) {
+      // Caso não, forçamos a Promise a ser rejeitada
+      return Promise.reject(response);
+    }
+
+    // Caso esteja tudo OK, lemos o body como JSON
+    return response.json();
+  })
+  .then((data) => {
+    // Por fim, escrevemos o body no console
+    console.log(data);
+  })
+  .catch((errorOrResponse) => {
+    // Em caso de falha simples (a request completou com um status diferente de 2xx)
+    // simplesmente logamos o status no console
+    if (errorOrResponse.status) {
+      return console.error(`Request failed with status ${errorOrResponse.status}`);
+    }
+
+    // Caso tenha acontecido um erro de rede (não foi possível completar a request)
+    // logamos o erro todo
+    console.error(errorOrResponse);
+  });
