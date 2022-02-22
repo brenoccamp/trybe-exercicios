3ae3fa1e306ba50dced63f1cbb9c15056745a09e
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-02-22 10:00:24 -0400

    Fix: now our function connect.execute is stubed and does not create new rows after each test runned

--- modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-04-arquitetura-de-software-testando-as-camadas/fixationExercises/tests/models/movieModel.test.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-nodeJS-camada-de-servico-e-arquitetura-rest-e-restful/dia-04-arquitetura-de-software-testando-as-camadas/fixationExercises/tests/models/movieModel.test.js
@@ -1,4 +1,7 @@
+const sinon = require('sinon');
 const { expect } = require('chai');
+
+const connection = require('../../models/connection');
 const MovieModels = require('../../models/movieModel');
 
 describe('Insert a new movie on DB', () => {
@@ -7,6 +10,17 @@ describe('Insert a new movie on DB', () => {
     directedBy: 'Jane Dow',
     releaseYear: 1999,
   };
+
+  before(async () => {
+    const execute = [{ insertedId: 1 }]; // retorno esperado nesse teste
+
+    sinon.stub(connection, 'execute').resolves(execute);
+  });
+
+  //Restauramos a função 'execute' original após os testes.
+  after(async () => {
+    connection.execute.restore();
+  });
 
   describe('when it is successfully entered', () => {
     it('it returns an object', async () => {
