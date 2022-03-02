28632609e70e751c83420d3278fae5af143e8ea0
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-02 16:02:42 -0400

    Add new column called "email" at migration from table Users.

--- modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/migrations/20220302194158-create-user.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/migrations/20220302194158-create-user.js
@@ -11,6 +11,9 @@ module.exports = {
       fullName: {
         type: Sequelize.STRING
       },
+      email: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
