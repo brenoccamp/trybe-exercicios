ed1878ee21987e80e35c0de873f858e63dcb0e48
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-02 16:08:32 -0400

    Create new migration with command: "npx sequelize model:generate --name employee --attributes fullName:string" just to fix knowledge

---
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/migrations/20220302200717-create-employee.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('employees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fullName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('employees');
+  }
+};
---
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/models/employee.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class employee extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  employee.init({
+    fullName: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'employee',
+  });
+  return employee;
+};
