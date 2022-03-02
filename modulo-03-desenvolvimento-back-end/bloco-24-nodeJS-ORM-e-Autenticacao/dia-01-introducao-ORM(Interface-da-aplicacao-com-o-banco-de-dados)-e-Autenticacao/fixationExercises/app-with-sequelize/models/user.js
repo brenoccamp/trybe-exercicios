7bd93de8a03757ba5a84f93059ab64d3eedddc60
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-02 15:46:47 -0400

    Trade content of file /models/user.js because for now we will only work with sequelize.define() function

--- modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/models/user.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/models/user.js
@@ -1,23 +1,10 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  User.init({
-    fullName: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
   });
+
   return User;
 };
+
+module.exports = User;
