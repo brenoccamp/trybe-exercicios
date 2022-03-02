e7e1b8e9c778495552cc0e4a80ef64f32cf28c9b
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-02 16:44:01 -0400

    Add content at functions up and down of file /migrations/add-column-phone-table-users.js

--- modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/migrations/20220302202352-add-column-phone-table-users.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/migrations/20220302202352-add-column-phone-table-users.js
@@ -2,20 +2,12 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.addColumn('Users', 'phone_num', {
+      type: Sequelize.STRING,
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.removeColumn('Users', 'phone_num');
   }
 };
