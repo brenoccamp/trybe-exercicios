68408c89e4f90209677984b9a269168ce7bd5e81
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-02 16:45:35 -0400

    Insert new column at file /models/user.js

--- modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/models/user.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/models/user.js
@@ -2,6 +2,7 @@ const User = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     fullName: DataTypes.STRING,
     email: DataTypes.STRING,
+    phone_num: DataTypes.STRING,
   });
 
   return User;
