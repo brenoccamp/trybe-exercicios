5d570f28969927585fd9240b5abb3b692ae36fca
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-02 21:35:22 -0400

    Add .get endpoint to list all users from table orm_example.users. Used sequelize query

--- modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/controllers/userController.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/controllers/userController.js
@@ -2,7 +2,17 @@ const express = require('express');
 const { User } = require('../models');
 const router = express.Router();
 
-// ...
+// Este endpoint usa o método findAll do Sequelize para retorno todos os users.
+router.get('/', async (_req, res) => {
+  try {
+    const users = await User.findAll();
+
+    return res.status(200).json(users);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
 
 // Este endpoint usa o método findByPk do Sequelize para buscar um usuário pelo id.
 router.get('/:id', async (req, res) => {
