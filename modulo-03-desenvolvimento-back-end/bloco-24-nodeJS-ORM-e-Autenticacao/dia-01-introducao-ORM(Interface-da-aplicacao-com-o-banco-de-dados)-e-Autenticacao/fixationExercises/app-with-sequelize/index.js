9590b6b5248ed9b1f1f1c488311df53c4cb3aa66
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-02 21:41:44 -0400

    Create index.js to listen on port 3000

---
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const userController = require('./controllers/userController');
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/user', userController);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
