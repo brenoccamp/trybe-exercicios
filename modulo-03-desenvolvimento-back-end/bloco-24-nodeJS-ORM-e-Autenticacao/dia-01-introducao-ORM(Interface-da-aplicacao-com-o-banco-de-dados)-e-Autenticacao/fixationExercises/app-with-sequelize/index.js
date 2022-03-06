9d40b0586729ace2d12df86159fadf869c0870b1
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-06 14:32:55 -0400

    put again directory /controllers inside paste src

--- modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/index.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/fixationExercises/app-with-sequelize/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const userController = require('./controllers/userController');
+const userController = require('./src/controllers/userController');
 
 const app = express();
 
