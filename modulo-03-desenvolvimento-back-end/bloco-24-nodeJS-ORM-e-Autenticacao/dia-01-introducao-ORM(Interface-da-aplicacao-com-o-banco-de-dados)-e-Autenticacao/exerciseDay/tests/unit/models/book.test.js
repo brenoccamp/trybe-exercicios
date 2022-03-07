13346fa09c130d76293affc70dfcce27830ed7a8
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-07 18:01:36 -0400

    Test unit for model Book

--- modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/exerciseDay/tests/unit/models/book.test.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-01-introducao-ORM(Interface-da-aplicacao-com-o-banco-de-dados)-e-Autenticacao/exerciseDay/tests/unit/models/book.test.js
@@ -4,3 +4,18 @@ const {
   checkModelName, 
   checkPropertyExists,
 } = require('sequelize-test-helpers');
+
+const BookModel = require('../../../models/book');
+
+describe('BookModel', () => {
+  const Book = BookModel(sequelize, dataTypes);
+  const book = new Book();
+
+  describe('has name Book', () => {
+    checkModelName(Book)('Book');
+  });
+
+  describe('have properties: "title", "author", "pageQuantity"', () => {
+    ["title", "author", "pageQuantity"].forEach(checkPropertyExists(book))
+  });
+});
