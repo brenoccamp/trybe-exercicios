fd79b2861f4c364d0765e2316bfa58fdc0be1eb7
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-07 20:11:45 -0400

    Add content to migration create-employees

--- modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-02-nodeJS-ORM-e-autenticacao/fixationExercise/migrations/20220308000212-create-employees.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-02-nodeJS-ORM-e-autenticacao/fixationExercise/migrations/20220308000212-create-employees.js
@@ -2,20 +2,31 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('employees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'first_name'
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'last_name'
+      },
+      age: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    }, {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('employees');
   }
 };
