ac5c5d12376237a542407440e189cc5b0175d985
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-09 22:14:31 -0400

    Create usersRouter and GET /login and install dependencies

---
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodeJS-ORM-e-Autenticacao/dia-03-JWT-JSON-Web-Token/dayExercise/hello-jwt/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const usersRouter = require('./routers/usersRouter');
+
+const { PORT } = process.env;
+
+const controllers = require('./controllers');
+const middlewares = require('./middlewares');
+
+const app = express();
+
+app.use(
+  cors({
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization'],
+  }),
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/ping', controllers.ping);
+
+app.use('/login', usersRouter);
+
+app.use(middlewares.error);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
