1f915d0b6a6c8bf40d0b109e1ba2fc50921ba042
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-17 07:00:37 -0400

    Create file index.js with route GET "/" and listen on PORT

---
+++ modulo-03-desenvolvimento-back-end/bloco-25-Deployment/dia-02-deploy-docker-e-heroku/app-heroku-ci-cd/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send(`<h1> Running on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Online: ${PORT}`));
