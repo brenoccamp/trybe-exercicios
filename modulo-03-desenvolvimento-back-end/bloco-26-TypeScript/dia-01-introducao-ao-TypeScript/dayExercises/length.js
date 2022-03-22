5f4d9cfb3661cd5d29b8e2a3dbc0d52a1689ac53
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-22 16:31:00 -0400

    Install node dependencies with npm install -D @tsconfig/node14 @types/node typescript

---
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-01-introducao-ao-TypeScript/dayExercises/length.js
@@ -0,0 +1,14 @@
+var units = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm'];
+function makeError(unity) {
+    throw new Error("A unidade ".concat(unity, " n\u00E3o \u00E9 uma unidade v\u00E1lida."));
+}
+function convert(value, baseUnity, conversionUnity) {
+    if (!units.includes(baseUnity))
+        makeError(baseUnity);
+    if (!units.includes(conversionUnity))
+        makeError(conversionUnity);
+    var baseIndex = units.indexOf(baseUnity);
+    var conversionIndex = units.indexOf(conversionUnity);
+    var expoent = (conversionIndex - baseIndex);
+    return value * (10 ^ expoent);
+}
