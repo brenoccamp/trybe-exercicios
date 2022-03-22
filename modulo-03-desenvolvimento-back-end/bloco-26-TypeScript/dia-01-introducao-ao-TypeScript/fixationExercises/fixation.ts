f2a08403d5197082be07705065193df3f4019967
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-22 10:57:23 -0400

    Fixation exercise! Created four enums: WeekDays, RainbowColors, Actions, CardinalPoints

---
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-01-introducao-ao-TypeScript/fixationExercises/fixation.ts
@@ -0,0 +1,34 @@
+enum WeekDays {
+  Sunday = 1,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+
+enum RainbowColors {
+  Red = 'Vermelho',
+  Orange = 'Laranja',
+  Yellow = 'Amarelo',
+  Green = 'Verde',
+  Blue = 'Azul',
+  Indigo = 'Anil',
+  Violet = 'Violeta',
+}
+
+enum Actions {
+  Save,
+  Print,
+  Open,
+  View,
+  Close,
+}
+
+enum CardinalPoints {
+  North = 'N',
+  East = 'E',
+  South = 'S',
+  West = 'W',
+}
