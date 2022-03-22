aa6df93909dee034163e2b31307efb2da212e639
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-22 13:44:55 -0400

    move files to folder exercisesFixation2.0

---
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-01-introducao-ao-TypeScript/fixationExercises/exercisesFixation2.0/index.ts
@@ -0,0 +1,17 @@
+import * as Exercise from './fixationExercises';
+
+console.log(Exercise.greeter('Maria'));
+console.log(Exercise.personAge('Maria', 40));
+console.log(`A soma do array é igual a ${Exercise.sumArray([3, 6, 9])}`);
+
+console.log(`Triângulo de base 10cm e altura 25cm: ${Exercise.triangle(10, 25)}cm²`);
+console.log(`Triângulo de base 5cm e altura 30cm: ${Exercise.triangle(5, 30)}cm²`);
+console.log(`Triângulo de base 100cm e altura 200cm: ${Exercise.triangle(10, 25)}cm²`);
+
+console.log(`Quadrado de lado 10cm: ${Exercise.square(10)}cm²`);
+console.log(`Quadrado de lado 5cm: ${Exercise.square(5)}cm²`);
+console.log(`Quadrado de lado 100cm: ${Exercise.square(10)}cm²`);
+
+console.log(`Retângulo de base 10cm e altura 25cm: ${Exercise.rectangle(10, 25)}cm²`);
+console.log(`Retângulo de base 5cm e altura 30cm: ${Exercise.rectangle(5, 30)}cm²`);
+console.log(`Retângulo de base 100cm e altura 200cm: ${Exercise.rectangle(10, 25)}cm²`);
