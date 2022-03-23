502199645583c8bb514ff78d9c8ee10b89b0677a
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-23 14:41:32 -0400

    Exercise 9 and 10 done! enums Months.ts and Seasons.ts

---
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-01-introducao-ao-TypeScript/dayExercises/Months.ts
@@ -0,0 +1,16 @@
+enum Months {
+  January = 'January',
+  February = 'February',
+  March = 'March',
+  April = 'April',
+  May = 'May',
+  June = 'June',
+  July = 'July',
+  August = 'August',
+  September = 'September',
+  October = 'October',
+  November = 'November',
+  December = 'December'
+}
+
+export default Months;
---
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-01-introducao-ao-TypeScript/dayExercises/Seasons.ts
@@ -0,0 +1,8 @@
+enum Seasons {
+  Fall = 'Fall',
+  Winter = 'Winter',
+  Summer = 'Summer',
+  Spring = 'Spring'
+}
+
+export default Seasons;
