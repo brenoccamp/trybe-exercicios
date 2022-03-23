f60567333ca317794a36f194e681cb2a462c5546
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-23 17:44:04 -0400

    Fixation exercises of type Aliases done!

--- modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-02-Tipagem-Estatica-e-Generics/fixationExercises/typeAliases/index.ts
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-02-Tipagem-Estatica-e-Generics/fixationExercises/typeAliases/index.ts
@@ -7,8 +7,17 @@ type Bird = {
 type Sum = (n1: number, n2: number) => number;
 
 type Address = {
+  state: string,
   city: string,
   street: string,
   district: string,
   houseNumber: number,
-}
+};
+
+type StatesOfMatter = 'liquid' | 'solid' | 'gaseous';
+
+type personId = string | number;
+
+type OperationSystem = 'linux' | 'mac' | 'windows';
+
+type AlphabetVowels = 'a' | 'e' | 'i' | 'o' | 'u';
