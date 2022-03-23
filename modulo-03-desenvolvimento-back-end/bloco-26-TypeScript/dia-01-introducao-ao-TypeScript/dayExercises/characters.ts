48a2ba5f80456afb030d7ef2971027711a0d27ed
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-23 09:35:53 -0400

    Exercise 6 done! Created characters.ts right typed

---
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-01-introducao-ao-TypeScript/dayExercises/characters.ts
@@ -0,0 +1,38 @@
+type character = {
+  nickname: string,
+  class: string,
+  stats: { agi: number, str: number, int: number, hp: number, mp: number},
+  createdAt: Date,
+};
+
+const characters: character[] = [
+  {
+    nickname: 'xKillerx',
+    class: 'warrior',
+    stats: { agi: 50, str: 100, int: 25, hp: 1000, mp: 300 },
+    createdAt: new Date('2003-10-1')
+  },
+  {
+    nickname: 'jainaProud',
+    class: 'mage',
+    stats: { agi: 80, str: 40, int: 150, hp: 630, mp: 1100 },
+    createdAt: new Date('2003-10-2')
+  },
+  {
+    nickname: 'catIn',
+    class: 'hunter',
+    stats: { agi: 150, str: 90, int: 80, hp: 800, mp: 600 },
+    createdAt: new Date('2003-10-15')
+  },
+]
+
+function printCharacter(character: any, index: number) {
+  const { nickname, class: cls, createdAt } = character;
+
+  console.log(`\n\n===== Character: ${index + 1} ========`);
+  console.log(`nickname: ${nickname}
+class: ${cls}
+createdAt: ${createdAt}`);
+}
+
+characters.forEach(printCharacter);
