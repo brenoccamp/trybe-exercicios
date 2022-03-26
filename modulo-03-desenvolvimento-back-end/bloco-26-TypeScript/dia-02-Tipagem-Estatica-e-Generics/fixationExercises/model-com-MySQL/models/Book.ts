3ff0da786a44abe715e472c3888453608f398023
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-26 12:56:17 -0400

    Created model create to create a new book on db

--- modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-02-Tipagem-Estatica-e-Generics/fixationExercises/model-com-MySQL/models/Book.ts
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-02-Tipagem-Estatica-e-Generics/fixationExercises/model-com-MySQL/models/Book.ts
@@ -1,4 +1,4 @@
-import { Pool } from 'mysql2/promise';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
 
 export interface Book {
   id?: number;
@@ -18,5 +18,15 @@ export default class BookModel {
   public async getAll(): Promise<Book[]> {
     const [result] = await this.connection.execute('SELECT * FROM books');
     return result as Book[];
+  }
+
+  public async create(book: Book): Promise<Book> {
+    const { title, price, author, isbn } = book;
+    const [result] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?);',
+      [title, price, author, isbn]
+    );
+    const { insertId } = result;
+    return { id: insertId, ...book };
   }
 }
