6ef3fe855a62953dc66e9173597e4fdf68de261c
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-26 13:20:51 -0400

    Created enum Doors.ts

---
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-02-Tipagem-Estatica-e-Generics/dayExercises/Doors.ts
@@ -0,0 +1,8 @@
+enum Doors {
+  DRIVER = 'Da pessoa motorista',
+  RIDE = 'Da pessoa carona',
+  BEHIND_DRIVER = 'De trás da pessoa motorista',
+  BEHIND_RIDE = 'De trás da pessoa carona'
+}
+
+export default Doors;
