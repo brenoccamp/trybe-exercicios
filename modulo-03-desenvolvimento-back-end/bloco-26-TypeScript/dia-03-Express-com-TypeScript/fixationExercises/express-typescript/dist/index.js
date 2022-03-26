be3bf8b8c20a8870bdef810b9add7f18e5216a7e
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-26 17:26:00 -0400

    Compiled to test

---
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-03-Express-com-TypeScript/fixationExercises/express-typescript/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_status_codes_1 = require("http-status-codes");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const PORT = 8000;
+app.get('/', (req, res) => {
+    res.status(http_status_codes_1.StatusCodes.OK).send('Express + TypeScript');
+});
+app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
