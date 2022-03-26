eb97826210fcf3b3b61abbda8fcc0f6b4396d99d
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-26 14:54:09 -0400

    Created some methods to CarClass

---
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-02-Tipagem-Estatica-e-Generics/dayExercises/CarClass.ts
@@ -0,0 +1,56 @@
+import Colors from './Colors';
+import Doors from './Doors';
+import Directions from './Directions';
+
+export default class Car {
+  _make: string;
+  _color: Colors;
+  _doors: number;
+
+  constructor(make: string, color: Colors, doors: number) {
+    this._make = make;
+    this._color = color;
+    this._doors = doors;
+  }
+
+  validateDoors(door: Doors): boolean|string {
+    if (Object.values(Doors).includes(door)) return true;
+    throw new Error('Porta inválida');
+  }
+
+  honk(): void {
+    console.log('Buzinando: BIP BIP');
+  }
+
+  openTheDoor(door: Doors): void {
+    console.log(`Abrindo a porta ${door}.`);
+  }
+
+  closeTheDoor(door: Doors): void {
+    console.log(`Fechando a porta ${door}.`);
+  }
+
+  turnOn(): void {
+    console.log('Carro ligado.');
+  }
+
+  turnOff(): void {
+    console.log('Carro desligado.');
+  }
+
+  speedUp(): void {
+    console.log('Acelerando carro.');
+  }
+
+  speedDown(): void {
+    console.log('Desacelerando carro.');
+  }
+
+  stop(): void {
+    console.log('Carro parado.');
+  }
+
+  turn(direction: Directions): void {
+    console.log(`Virando para a ${direction}.`);
+  }
+}
