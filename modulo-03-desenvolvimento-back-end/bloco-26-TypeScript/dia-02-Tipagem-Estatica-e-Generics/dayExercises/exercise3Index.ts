f2f72da940b323272c97fdae6ee145511c0de7f3
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-26 15:25:21 -0400

    Exercise 3 done! Created 3 obj(pizzas) usnig Pizza interface

---
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-02-Tipagem-Estatica-e-Generics/dayExercises/exercise3Index.ts
@@ -0,0 +1,22 @@
+import Pizza from './Pizza';
+
+const calabresa: Pizza = {
+  flavor: 'Calabresa',
+  slices: 8
+};
+
+console.log(calabresa);
+
+const marguerita: Pizza = {
+  flavor: 'Marguerita',
+  slices: 6,
+};
+
+console.log(marguerita);
+
+const nutela: Pizza = {
+  flavor: 'Nutela',
+  slices: 4,
+};
+
+console.log(nutela);
