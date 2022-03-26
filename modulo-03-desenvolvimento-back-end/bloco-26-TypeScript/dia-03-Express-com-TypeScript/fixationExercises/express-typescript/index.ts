bbc55dfefac4640ae8003019d8ebadaa30c54067
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-26 17:31:17 -0400

    Implemented error middleware typed

--- modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-03-Express-com-TypeScript/fixationExercises/express-typescript/index.ts
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-03-Express-com-TypeScript/fixationExercises/express-typescript/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 const app = express();
@@ -9,6 +9,28 @@ const PORT = 8000;
 
 app.get('/', (req: Request, res: Response) => {
   res.status(StatusCodes.OK).send('Express + TypeScript');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const { name, message, details } = err as any;
+  console.log(`name: ${name}`);
+
+  switch(name) {
+    case 'ValidationError':
+      res.status(400).json({ message: details[0].message });
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
+    case 'ConflitError':
+      res.status(409).json({ message });
+      break;
+    default:
+      console.error(err);
+      res.sendStatus(500);
+  }
+
+  next();
 });
 
 app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
