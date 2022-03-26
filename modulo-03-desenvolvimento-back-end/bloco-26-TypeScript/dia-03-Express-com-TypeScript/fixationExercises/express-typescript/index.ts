4c8d4c7435ec7cd6b708508d41bfdd3faa945def
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-26 17:26:19 -0400

    Refactored index.ts to using now types

--- modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-03-Express-com-TypeScript/fixationExercises/express-typescript/index.ts
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-03-Express-com-TypeScript/fixationExercises/express-typescript/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 const app = express();
@@ -7,7 +7,7 @@ app.use(express.json());
 
 const PORT = 8000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.status(StatusCodes.OK).send('Express + TypeScript');
 });
 
