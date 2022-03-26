8f16cfb9bbc5acd4d4afa29fe02b59cfbe45e4b5
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-26 17:44:42 -0400

    Re-run command npm start to re-build and re-compile all files

--- modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-03-Express-com-TypeScript/fixationExercises/express-typescript/dist/index.js
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-03-Express-com-TypeScript/fixationExercises/express-typescript/dist/index.js
@@ -5,10 +5,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const http_status_codes_1 = require("http-status-codes");
+const BooksRoutes_1 = __importDefault(require("./routers/BooksRoutes"));
+require("express-async-errors");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const PORT = 8000;
 app.get('/', (req, res) => {
     res.status(http_status_codes_1.StatusCodes.OK).send('Express + TypeScript');
+});
+app.use(BooksRoutes_1.default);
+app.use((err, req, res, next) => {
+    const { name, message, details } = err;
+    console.log(`name: ${name}`);
+    switch (name) {
+        case 'ValidationError':
+            res.status(400).json({ message: details[0].message });
+            break;
+        case 'NotFoundError':
+            res.status(404).json({ message });
+            break;
+        case 'ConflitError':
+            res.status(409).json({ message });
+            break;
+        default:
+            console.error(err);
+            res.sendStatus(500);
+    }
+    next();
 });
 app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
