d0fd9a9455c7a1b8953e35c677cc29f087a60576
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-26 15:12:19 -0400

    Created a script to simulate a run day of a driver

---
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-02-Tipagem-Estatica-e-Generics/dayExercises/index.ts
@@ -0,0 +1,36 @@
+import CarClass from './CarClass';
+import Colors from './Colors';
+import Directions from './Directions';
+import Doors from './Doors';
+
+const gol = new CarClass('Volkswagem', Colors.SILVER, 4);
+
+gol.openTheDoor(Doors.DRIVER);
+gol.closeTheDoor(Doors.DRIVER);
+gol.turnOn();
+gol.speedUp();
+gol.turn(Directions.LEFT);
+gol.speedUp();
+gol.speedDown();
+gol.turn(Directions.RIGHT);
+gol.speedUp();
+gol.turn(Directions.RIGHT);
+gol.speedDown();
+gol.stop();
+gol.openTheDoor(Doors.BEHIND_RIDE);
+gol.closeTheDoor(Doors.BEHIND_RIDE);
+gol.speedUp();
+gol.speedDown();
+gol.turn(Directions.RIGHT);
+gol.speedUp();
+gol.speedDown();
+gol.turn(Directions.LEFT);
+gol.speedUp();
+gol.speedDown();
+gol.turn(Directions.RIGHT);
+gol.speedUp();
+gol.speedDown();
+gol.stop();
+gol.openTheDoor(Doors.BEHIND_RIDE);
+gol.closeTheDoor(Doors.BEHIND_RIDE);
+gol.speedUp();
