5b6d3d4d925b279be4b4085527bf55f28ba6b6eb
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-26 15:26:03 -0400

    Edited: now slices isnt using type Slices, but declared within obj

--- modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-02-Tipagem-Estatica-e-Generics/dayExercises/Pizza.ts
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-02-Tipagem-Estatica-e-Generics/dayExercises/Pizza.ts
@@ -1,6 +1,6 @@
-type Slices = 4 | 6 | 8;
+// type Slices = 4 | 6 | 8; -> Podemos declarar um tipo para slices ou colocar direto na interface.
 
 export default interface Pizza {
   flavor: string;
-  slices: Slices;
+  slices: 4 | 6 | 8; // -> aqui pderíamos colocar apenas Slices em vez de 4 | 6 | 8;
 }
