aad8d7c54fda1eab49bb64b5257e78acbd004eb3
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-26 15:36:38 -0400

    Created Types of new pizzas

---
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-02-Tipagem-Estatica-e-Generics/dayExercises/PizzaFlavorsTypes.ts
@@ -0,0 +1,9 @@
+type Comum = 'Calabresa' | 'Franco' | 'Peperoni';
+type Vegeratiana = 'Marguerita' | 'Palmito' | 'Cogumelo';
+type Doce = 'Nutela' | 'Goiabada com Queijo' | 'Marshmallow';
+
+export {
+  Comum,
+  Vegeratiana,
+  Doce,
+};
