ef8b577b42e0ea04c1e8288a12d4104c776b298c
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-28 20:18:58 -0400

    Implemented error middleware to return message of error for user

--- modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-03-Express-com-TypeScript/dayExercises/exercise1/src/middlewares/error.ts
+++ modulo-03-desenvolvimento-back-end/bloco-26-TypeScript/dia-03-Express-com-TypeScript/dayExercises/exercise1/src/middlewares/error.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 
 const error = (err: any, _req: Request, res: Response, _next: NextFunction): Response<string> => {
+  if (err.isJoi) {
+    return res.status(400).json({ message: err.details[0].message });
+  }
   console.error(err);
   return res.status(500).json({ message: 'Internal server error' });
 };
