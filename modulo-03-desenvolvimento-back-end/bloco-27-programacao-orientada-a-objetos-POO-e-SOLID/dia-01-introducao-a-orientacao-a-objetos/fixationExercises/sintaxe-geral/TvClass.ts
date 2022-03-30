1140b3524220d2ca1def714a8d033cc8b8c2937f
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-30 10:02:55 -0400

    Fixation Exercise: created a class called Tv and instanciated it. Also start a node.js project and created .gitignore

---
+++ modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-POO-e-SOLID/dia-01-introducao-a-orientacao-a-objetos/fixationExercises/sintaxe-geral/TvClass.ts
@@ -0,0 +1,29 @@
+class Tv {
+  brand: string;
+  size: number;
+  resolution: string;
+  connections: string[];
+  connectedTo: string;
+
+  constructor(brand: string, size: number, resol: string, connec: string[]) {
+    console.log(`Creating a new ${brand} Tv!`);
+    this.brand = brand;
+    this.size = size;
+    this.resolution = resol;
+    this.connections = connec;
+  }
+
+  turnOn(): void {
+    console.log(`Turn on the ${this.brand} size ${this.size}"
+    with resolution ${this.resolution} and available connections ${this.connections}`);
+  }
+}
+
+const samsungTv = new Tv(
+  'Samsung',
+  42,
+  '4k',
+  ['HDMI', 'Ethernet', 'Wi-fi', 'Bluetooth'],
+);
+
+samsungTv.turnOn();
