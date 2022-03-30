e73c2b427e65b2098a3fda37ccae9fa6d8d994d1
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-30 15:05:45 -0400

    Created MailListClass containing many methods

---
+++ modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-POO-e-SOLID/dia-01-introducao-a-orientacao-a-objetos/fixationExercises/emailFixationExercise/MailListClass.ts
@@ -0,0 +1,25 @@
+import Email from './EmailClass';
+
+class MailList {
+  constructor(private mailList: Email[] = []) { }
+
+  get all(): Email[] { return this.mailList; }
+
+  getByEmailFrom(mailAddress: string): Email[] {
+    return this.mailList.filter((mail) => mail.from === mailAddress);
+  }
+
+  getByEmailTo(mailAddress: string): Email[] {
+    return this.mailList.filter((mail) => mail.to === mailAddress);
+  }
+
+  getBySubject(searchString: string): Email[] {
+    return this.mailList.filter((mail) => mail.subject.indexOf(searchString) !== -1);
+  }
+
+  addEmail(newMail: Email): void { this.mailList.push(newMail); }
+
+  removeEmail(mailToRemove: Email): void {
+    this.mailList = this.mailList.filter((mail) => mail !== mailToRemove);
+  }
+} 
