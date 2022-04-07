5b436fd503f85f4ee2ed489150539d18034c710b
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-04-07 15:11:42 -0400

    First fixation exercise done! Refactored the code of fixationSRP to fix all eslint problems

---
+++ modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-POO-e-SOLID/dia-04-SOLID-introducao-e-principios-s,o,d/fixationExercises/fixationSRP.ts
@@ -0,0 +1,54 @@
+// ReadingTracker.ts
+// Observe o código abaixo e faça as alterações necessárias
+// para que ele passe a respeitar o Princípio da Responsabilidade Única (SRP).
+
+type Book = {
+  book: string;
+  author: string;
+  genre: string;
+}
+
+class ReadingTracker {
+  private readingGoal: number;
+
+  private booksRead: number;
+
+  private wishlist: Book[];
+
+  constructor(readingGoal: number) {
+    this.readingGoal = readingGoal;
+    this.booksRead = 0;
+    this.wishlist = [];
+  }
+
+  trackReadings(readsCount: number) {
+    this.booksRead += readsCount;
+    if (this.booksRead >= this.readingGoal) {
+      this.progressNotification(
+        'Congratulations! You\'ve reached your reading goal!',
+      );
+      return;
+    }
+    this.progressNotification(
+      'There are still some books to go!',
+    );
+  }
+
+  addToWishList(book: Book): void {
+    this.wishlist.push(book);
+  }
+
+  showWishlist(): void {
+    console.log(this.wishlist);
+  }
+
+  progressNotification = (message: string): void => {
+    console.log(message);
+  }
+}
+
+const readTracker = new ReadingTracker(20);
+readTracker.addToWishList({ book: 'The Road', author: 'Cormac McCarthy', genre: 'Dystopia' });
+readTracker.showWishlist();
+readTracker.trackReadings(12);
+readTracker.trackReadings(9);
