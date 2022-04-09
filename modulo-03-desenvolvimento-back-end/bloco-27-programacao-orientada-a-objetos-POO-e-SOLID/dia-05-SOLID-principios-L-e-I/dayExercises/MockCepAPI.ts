fa4f76698e0d376f81316c92c2773ff6caa3aa83
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-04-09 11:45:58 -0400

    Exercise 03 done! Created a class called MockCepAPI

---
+++ modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-POO-e-SOLID/dia-05-SOLID-principios-L-e-I/dayExercises/MockCepAPI.ts
@@ -0,0 +1,9 @@
+export default class MockCepAPI {
+  async getAddressByCEP(cep: string, number: number): Promise<string> {
+    return `O endereço para o "CEP:${cep}, n°:${number}" é "endereço foo"`;
+  }
+
+  async getCepByAddress(address: string, number:number): Promise<string> {
+    return `O CEP para: "${address}, n°: ${number}" é "CEP baz"`;
+  }
+}
