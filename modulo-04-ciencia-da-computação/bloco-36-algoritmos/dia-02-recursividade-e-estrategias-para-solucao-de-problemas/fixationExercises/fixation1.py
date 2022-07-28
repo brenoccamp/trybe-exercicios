e1e80bb44ae17fe3e4f74b2963b0b976fae8434f
brenoccamp <brenoccamp@gmail.com>
2022-07-27 20:01:30 -0400

    Refactor exercise1

--- modulo-04-ciencia-da-computação/bloco-36-algoritmos/dia-02-recursividade-e-estrategias-para-solucao-de-problemas/fixationExercises/fixation1.py
+++ modulo-04-ciencia-da-computação/bloco-36-algoritmos/dia-02-recursividade-e-estrategias-para-solucao-de-problemas/fixationExercises/fixation1.py
@@ -1,10 +1,22 @@
 # Exercício: Faça um algoritmo recursivo de soma. Esse algoritmo deve receber um número de parâmetro e deve somar todos os números antecessores a ele.
 
 
-def recursive_sum(n):
-    if n == 0:
-        return n
-    return n + recursive_sum(n - 1)
+# def recursive_sum(n):
+#     if n == 0:
+#         return n
+#     return n + recursive_sum(n - 1)
 
 
-print(recursive_sum(4))
+# print(recursive_sum(4))
+
+
+def iterative_factorial(n):
+    fact = 1
+    for i in range(1, n):
+        fact *= i
+        print('i: ', i)
+    print(fact)
+    return fact
+
+
+iterative_factorial(5)
